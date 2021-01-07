6a61df30d71e17a6cf71280fc1e1a6928a10efc7
Arul Ashri <arulashri22@gmaill.com>
2021-01-07 13:15:25 +0530

    added pdf

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,50 +1,48 @@
-import React ,{useState,useEffect}from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import { Card, CardTitle, CardBody, CardText, Button } from 'reactstrap';
 import { Input } from 'reactstrap';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 
 
 
-const CartComponent = ({setCart,cart}) => {
-    
+
+const CartComponent = ({ setCart, cart }) => {
+
     const [place, setPlace] = useState(false);
     const [name, setName] = useState("");
-
-    
-
-    const [order, setOrder] = useState({
-        name: "",
-        table: "",
-        list: [],
-      });
-
-    useEffect(() => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title: 'React POST Request Example' })
-        };
-        const apiUrl = `https://debug.kyukey.tech/api/dining/new_order/`;
-        fetch(apiUrl, requestOptions)
-             .then((res) => res.json())
-             .then(data => {
-                 setOrder({name: {name} ,table: "4c930b1f-f733-46c6-ba74-6bde6105b8d3", list: {cart},})
-             });
-    },[setOrder,cart,name]);
+    const [pdfDis, setPdfDis] = useState(false);
 
 
-    const RemoveCart = (Toremove) =>{
+
+    // useEffect(() => {
+    //     const requestOptions = {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify({ title: 'React POST Request Example' })
+    //     };
+    //     const apiUrl = `https://debug.kyukey.tech/api/dining/new_order/`;
+    //     fetch(apiUrl, requestOptions)
+    //          .then((res) => res.json())
+    //          .then(data => {
+    //              setOrder({name: {name} ,table: "4c930b1f-f733-46c6-ba74-6bde6105b8d3", list: {cart},})
+    //          });
+    // },[]);
+
+
+    const RemoveCart = (Toremove) => {
         setCart(
             cart.filter((data) => data !== Toremove)
         );
     }
 
 
-    const Decrease = (data) =>{
+    const Decrease = (data) => {
         setCart(cart.map(
             (item) => {
-                if (item.id === data.id){
+                if (item.id === data.id) {
                     item.quantity--;
                     return item;
                 }
@@ -53,10 +51,10 @@ const CartComponent = ({setCart,cart}) => {
         ));
     }
 
-    const AddMore = (data) =>{
+    const AddMore = (data) => {
         setCart(cart.map(
             (item) => {
-                if (item.id === data.id){
+                if (item.id === data.id) {
                     item.quantity++;
                     return item;
                 }
@@ -69,119 +67,190 @@ const CartComponent = ({setCart,cart}) => {
         <div className="row justify-content-center">
             <div className="mt-2 col-9">
                 <Card className="saladItem">
-                <CardBody>
-                    <CardTitle tag="h3" className="row"><div  className="col-8">{data.name}</div> <div className="col">x {data.quantity} </div></CardTitle>
-                    <div className="row"> 
-                        <div className="col-7"> <CardText tag="h3">₹{data.price}</CardText> </div>
-                        {data.quantity === 1 && <div className="col-5"> <Button color="danger"  onClick={() =>RemoveCart(data)}> Remove</Button> </div>}
-                        {data.quantity > 1 && <div className="col-5"> <Button className="mr-2" color="danger" onClick={() =>Decrease(data)}> -</Button> 
-                        <Button color="danger"  onClick={() =>AddMore(data)}> +</Button></div>}
-                    </div>
-                   
-                </CardBody>
-                
+                    <CardBody>
+                        <CardTitle tag="h3" className="row"><div className="col-8">{data.name}</div> <div className="col">x {data.quantity} </div></CardTitle>
+                      {!pdfDis && (  <div className="row">
+                            <div className="col-7"> <CardText tag="h3">₹{data.price}</CardText> </div>
+                            {data.quantity === 1 && <div className="col-5"> <Button color="danger" onClick={() => RemoveCart(data)}> Remove</Button> </div>}
+                            {data.quantity > 1 && <div className="col-5"> <Button className="mr-2" color="danger" onClick={() => Decrease(data)}> -</Button>
+                                <Button color="danger" onClick={() => AddMore(data)}> +</Button></div>}
+                        </div>)}
+
+                    </CardBody>
+ 
                 </Card>
                 <hr className="my-2" />
             </div>
             <div className="mt-2 col-md-3">
-                
+
             </div>
         </div>
-            
+
     );
 
-    var CartList = cart.map(data =>{
-        return(
-            <div  > 
-             <CartItem data={data} key={data.id} />
+    var CartList = cart.map(data => {
+        return (
+            <div  >
+                <CartItem data={data} key={data.id} />
             </div>
 
         );
     });
 
-    var tot=0;
-    var total = cart.map(data =>{
-        tot= tot + (data.quantity*data.price);
+    var tot = 0;
+    var total = cart.map(data => {
+        tot = tot + (data.quantity * data.price);
         return tot;
     })
 
 
     const TotalRender = () => {
-        return(
+        return (
             <div className="container">
-            <div className="row justify-content-center">
-        <div className="col-8 col-offset">
-            <Card className="saladItem">
-                <CardBody align="center">
-                    <CardTitle tag="h3">Total</CardTitle>
-                    <h5> ₹ {tot}</h5>
-                </CardBody>
-            </Card>
-        </div>
-        </div>
-        </div>
+                <div className="row justify-content-center">
+                    <div className="col-8 col-offset">
+                        <Card className="saladItem">
+                            <div>
+                                <CardBody className="row"> 
+                                   <div className="col-5 offset-sm-2 align-self-center mr-0"><CardTitle tag="h3">Total - </CardTitle></div> 
+                                   <div className="col-5  align-self-center"><h5 > ₹ {tot}</h5></div> 
+                                </CardBody>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+            </div>
 
-        )}
-    
-        const CartRender = () =>{
-         if(!place){
-            return(
-                <div>
-                    <div className="saladCard mt-5 pb-5"> 
-                        <h1 align="center">Cart ({cart.length})</h1>
+        )
+    }
+
+    const CartRender = () => {
+      
+            return (
+                <div className="pb-3">
+                    <div className="saladCard mt-5 pb-2">
+                       {!pdfDis && ( <h1 align="center">Cart ({cart.length})</h1>)}
+                       {pdfDis && ( <h2 align="center" className="m-1">ORDER SUMMARY</h2>)}
                         <div className="container">
                             {CartList}
                         </div>
                         <div>
                             <TotalRender />
                         </div>
+                        <div className="mt-4 mb-2">
+                            <div align="center">
+                                {!pdfDis &&  ( <Button color="primary" onClick={() => setPlace(true)}>PROCEED</Button>)}
+                                {pdfDis && ( <Button color="primary" onClick={() => pdfDownload()}>Download</Button>)}
+                            </div>
+                        </div>
                     </div>
-                    <div align="center">
-                       <Button color="primary" onClick={() =>setPlace(true)}>PROCEED</Button>
-                    </div>
-                </div> 
-            )}
-            return(<div></div>)
+                    
+                </div>
+            )
+    
 
-        }
-        const PlaceOrder = () =>{
-            console.log({order})
-        }
+    }
+    const PlaceOrder = () => {
+        setPdfDis(true);
+        var items = cart.map((e) => { return { menuitem: e.id, quantity: e.quantity } });
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ table: '4c930b1f-f733-46c6-ba74-6bde6105b8d3', items: items, customer_name: name })
+        };
+        const apiUrl = `https://debug.kyukey.tech/api/dining/new_order/`;
+        fetch(apiUrl, requestOptions)
+            .then((res) => null)
+            .catch((err) => alert('Error'));
 
         
+    }
 
-        const NameRender = () =>{
-            if(place){
-                return(
-                    <div>
-                        <div className="saladCard mt-5 pb-5">
-                            <h3 align="center">Enter Your Name</h3>
-                            <Input placeholder="your name.." value={name} onChange={e => setName(e.target.value)} />
-                        </div>
-                        <div align="center">
-                          <Button color="danger" onClick={() =>setPlace(false)}>Cancel</Button>
-                          <Button color="success" onClick={() =>PlaceOrder()}>Place Order</Button>
-                        </div>
-                    </div>
-                )
+
+
+    // const NameRender = () =>{
+    //     if(place){
+    //         return(
+    //             <div key="input_name" >
+    //                 <div className="saladCard mt-5 pb-5">
+    //                     <h3 align="center">Enter Your Name</h3>
+    //                     <Input placeholder="your name.." value={name} onChange={e => setName(e.target.value)} />
+    //                 </div>
+    //                 <div align="center">
+    //                   <Button color="danger" onClick={() =>setPlace(false)}>Cancel</Button>
+    //                   <Button color="success" onClick={() =>PlaceOrder()}>Place Order</Button>
+    //                 </div>
+    //             </div>
+    //         )
+    //     }
+    //     return(<div></div>)
+    // }
+
+
+    function pdfDownload() {
+        var bod = document.body;
+        var HTML_Width = bod.clientWidth;
+        var HTML_Height = bod.clientHeight;
+        var top_left_margin = 15;
+        var PDF_Width = HTML_Width + (top_left_margin * 2);
+        var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+    
+        var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+    
+        html2canvas(bod).then(function (canvas) {
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+            for (var i = 1; i <= totalPDFPages; i++) {
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
             }
-            return(<div></div>)
-        }
-       
+            pdf.save("order_summary.pdf");
+        });
+    }
 
-      
+
+
     return (
         <div className="container cart">
             <div className="float row" align="center">
-                    <Link className="ml-3" to="/"><h5>.<span className="fa fa-home fa-lg my-float " aria-hidden="true"></span></h5></Link>
+                <Link className="ml-3" to="/"><h5>.<span className="fa fa-home fa-lg my-float " aria-hidden="true"></span></h5></Link>
             </div>
             <div className="container">
-               <CartRender />
-               <NameRender />
+                {!place && (<CartRender />)}
+                {/* <NameRender /> */}
+                {place && !pdfDis && (
+
+                    <div key="input_name"  className="pb-5">
+                        <div className="saladCard mt-5">
+                            <h3 align="center">Enter Your Name</h3>
+                            <div className="pl-3 pr-3">
+                                 <Input placeholder="your name.." value={name} onChange={e => setName(e.target.value)} />
+                            </div>
+                            <div className="mt-2 mb-2">
+                                  <div align="center">
+                                    <Button className="m-3" color="danger" onClick={() => setPlace(false)}>Cancel</Button>
+                                    <Button color="success" onClick={() => PlaceOrder()}>Place Order</Button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                )}
+                {pdfDis && (
+                    <div>
+                    <CartRender />
+                    </div>
+                 
+                   
+                )}
             </div>
-            
+
         </div>
-    
+
     )
 }
 
